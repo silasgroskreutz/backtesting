484f261369c0d0181998970bdb00f758d47c00d7
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2021-10-17 20:23:39 -0500

    Completed ichimoku module with calculations for all signals now in the module

--- strategies/ichimoku.py
+++ strategies/ichimoku.py
@@ -12,6 +12,51 @@ def backtest(df: pd.dataframe, tekan_period: int, kijun_period: int):
     # Tenkan Sen : Short-Term signal line
 
     df["rolling_min_tenkan"] = df["low"].rolling(window=tenkan_period).min()
-    df["rolling_min_tenkan"] = df["high"].rolling(window=tenkan_period).max()
+    df["rolling_max_tenkan"] = df["high"].rolling(window=tenkan_period).max()
 
     df["tenkan_sen"] = (df["rolling_max_tenkan"] + df["rolling_min_tenkan"]) / 2
+
+    df.drop(["rolling_min_tenkan", "rolling_max_tenkan"], axis=1, inplace=True)
+
+    # Kijun Sen : Short-Term signal line
+
+    df["rolling_min_kijun"] = df["low"].rolling(window=kijun_period).min()
+    df["rolling_max_kijun"] = df["high"].rolling(window=kijun_period).max()
+
+    df["kijun_sen"] = (df["rolling_max_kijun"] + df["rolling_min_kijun"]) / 2
+
+    df.drop(["rolling_min_kijun", "rolling_max_kijun"], axis=1, inplace=True)
+
+    # Senkou Span A
+
+    df["senkou_span_a"] = ((df["tenkan_sen"] + df["kijun_sen"]) / 2).shift(kijun_period)
+
+    # Senkou Span B
+    df["rolling_min_senkou"] = df["low"].rolling(window=kijun_period * 2).min()
+    df["rolling_max_senkou"] = df["high"].rolling(window=kijun_period * 2).max()
+
+    df["senkou_span_b"] = ((df["rolling_max_senkou"] + df["rolling_min_senkou"]) / 2).shift(kijun_period)
+
+    df.drop(["rolling_min_senkou", "rolling_max_senkou"], axis=1, inplace=True)
+
+    # Chikou Span : Confirmation line
+
+    df["chikou_span"] = df["close"].shift(kijun_period)
+
+    df.dropna(inplace=True)
+
+    # Signal
+
+    df["tenkan_minus_kijun"] = df["tenkan_sen"] - df["kijun_sen"]
+    df["prev_tenkan_minus_kijun"] = df["tenkan_minus_kijun"].shift(1)
+
+    df["signal"] = np.where((df["tenkan_minus_kijun"] > 0) & (df["prev_tenkan_minus_kijun"] < 0) & (df["close"] > df["senkou_span_a"]) & (df["close"] > df["senkou_span_b"]) & (df["close"] >df["chikou_span"]), 1, np.where((df["tenkan_minus_kijun"] < 0) & (df["prev_tenkan_minus_kijun"] > 0) & (df["close"] < df["senkou_span_a"]) & (df["close"] < df["senkou_span_b"]) & (df["close"] < df["chikou_span"]), -1, 0))
+
+    signal_data = df[df["signal"] != 0]/copy()
+
+    signal_data["pnl"] = signal_data["close"].pct.change() * signal_data["signal"].shift(1)
+
+    return signal_data["pnl"].sum()
+
+
+
